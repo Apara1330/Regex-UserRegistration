97988912388515a14eca23dd0d9bbbf9f40fe1de
Apara1330 <polapara1330@gmail.com>
2022-03-06 14:37:28 +0530

    [Apara] -Validation for Mobile number done

--- Regex-UserRegistration/Regex-UserRegistration/Program.cs
+++ Regex-UserRegistration/Regex-UserRegistration/Program.cs
@@ -12,17 +12,22 @@ namespace Regex_UserRegistration
         {
             //Console.WriteLine("Hello..");
             ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
-            Console.WriteLine("Enter valid first name : ");
+            /*Console.WriteLine("Enter valid first name : ");
             string firstName = Console.ReadLine();
             validateUserRegistration.ValidateFirstName(firstName);
-
-            Console.WriteLine("Enter valid last name : ");
+*/
+            /*Console.WriteLine("Enter valid last name : ");
             string lastName = Console.ReadLine();
-            validateUserRegistration.ValidateLastName(lastName);
+            validateUserRegistration.ValidateLastName(lastName);*/
 
-            Console.WriteLine("Enter valid EmailId");
+           /* Console.WriteLine("Enter valid EmailId");
             string mail = Console.ReadLine();
-            validateUserRegistration.ValidateEmail(mail);
+            validateUserRegistration.ValidateEmail(mail);*/
+            
+
+            Console.WriteLine("Enter mobile number");
+            string mnum = Console.ReadLine();
+            validateUserRegistration.ValidateMobile(mnum);
             Console.ReadLine();
         }
     }
--- Regex-UserRegistration/Regex-UserRegistration/ValidateUserRegistration.cs
+++ Regex-UserRegistration/Regex-UserRegistration/ValidateUserRegistration.cs
@@ -12,6 +12,7 @@ namespace Regex_UserRegistration
         public const string First_Name = "^[A-Z][a-zA-Z]{2}";//first namepattern
         public const string Last_Name = "^[A-Z][a-zA-Z]{2}";//last name pattern
         public const string Email = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";//email pattern
+        public const string Mobile = "^([9][1])+[ ]+[6789]{1}[0-9]{9}$";
 
 
 
@@ -55,6 +56,20 @@ namespace Regex_UserRegistration
                 Console.WriteLine("Email does not match with the pattern");
             }
 
+        }
+        public void ValidateMobile(string MobNum)
+        {
+            
+            Regex regex = new Regex(Mobile);
+
+            if (regex.IsMatch(MobNum))
+            {
+                Console.WriteLine("Mobile Number is matched with pattern");
+            }
+            else
+            {
+                Console.WriteLine("Mobile Number does not match with pattern");
+            }
         }
     }
          
