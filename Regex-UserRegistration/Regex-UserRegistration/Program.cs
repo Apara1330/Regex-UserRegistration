2fab90f725c3ef4713fd00295ea8a678fc110d9a
Apara1330 <polapara1330@gmail.com>
2022-03-06 13:57:12 +0530

    [Apara]- First name validation done

---
+++ Regex-UserRegistration/Regex-UserRegistration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_UserRegistration
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello..");
+            ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
+            Console.WriteLine("Enter valid first name : ");
+            string firstName = Console.ReadLine();
+            validateUserRegistration.ValidateFirstName(firstName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Regex-UserRegistration/Regex-UserRegistration/ValidateUserRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_UserRegistration
+{
+    public class ValidateUserRegistration
+    {
+        public const string First_Name = "^[A-Z][a-zA-Z]{2}";
+
+        public void ValidateFirstName(string firstName)
+        {
+            Regex regex = new Regex(First_Name);
+
+            if (regex.IsMatch(firstName))
+            {
+                Console.WriteLine("First name is matching with the pattern");
+            }
+            else
+            {
+                Console.WriteLine("First name is not matching with the pattern");
+            }
+        }
+    }
+}
